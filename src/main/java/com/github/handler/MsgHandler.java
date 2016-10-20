91f37cf1632ea0c817b8fb3106129d35bacd945b
FirenzesEagle <liumingbo2008@gmail.com>
2016-10-20 11:41:10 +0800

    update message handler

--- src/main/java/com/github/handler/MsgHandler.java
+++ src/main/java/com/github/handler/MsgHandler.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 import java.util.Map;
 
 /**
- * 转发客户消息Handler
+ * 转发客户消息给客服Handler
  *
  * Created by FirenzesEagle on 2016/7/27 0027.
  * Email:liumingbo2008@gmail.com
