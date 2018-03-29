0e5d5ceba225b2fa6444fad2e7582ef910c589a6
Binary Wang <binarywang@gmail.com>
2018-03-29 11:08:31 +0800

    优化代码

--- src/main/java/com/github/weixin/demo/controller/CoreController.java
+++ src/main/java/com/github/weixin/demo/controller/CoreController.java
@@ -3,7 +3,6 @@ package com.github.weixin.demo.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.github.weixin.demo.util.ReturnModel;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.github.weixin.demo.service.CoreService;
-
+import com.github.weixin.demo.util.ReturnModel;
 import me.chanjar.weixin.common.exception.WxErrorException;
 import me.chanjar.weixin.mp.api.WxMpConfigStorage;
 import me.chanjar.weixin.mp.api.WxMpService;
@@ -34,19 +33,15 @@ public class CoreController extends GenericController {
     @Autowired
     protected CoreService coreService;
 
-    @RequestMapping(value = "index")
+    @RequestMapping(value = "/index")
     public String index() {
         return "index";
     }
 
     /**
      * 微信公众号webservice主服务接口，提供与微信服务器的信息交互
-     *
-     * @param request
-     * @param response
-     * @throws Exception
      */
-    @RequestMapping(value = "core")
+    @RequestMapping(value = "/core")
     public void wechatCore(HttpServletRequest request, HttpServletResponse response) throws Exception {
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
@@ -77,7 +72,11 @@ public class CoreController extends GenericController {
             // 明文传输的消息
             WxMpXmlMessage inMessage = WxMpXmlMessage.fromXml(request.getInputStream());
             WxMpXmlOutMessage outMessage = this.coreService.route(inMessage);
-            response.getWriter().write(outMessage.toXml());
+            if (outMessage == null) {
+                response.getWriter().write("");
+            } else {
+                response.getWriter().write(outMessage.toXml());
+            }
             return;
         }
 
@@ -89,25 +88,26 @@ public class CoreController extends GenericController {
                 msgSignature);
             this.logger.debug("\n消息解密后内容为：\n{} ", inMessage.toString());
             WxMpXmlOutMessage outMessage = this.coreService.route(inMessage);
-            this.logger.info(response.toString());
-            response.getWriter()
-                .write(outMessage.toEncryptedXml(this.configStorage));
+            if (outMessage == null) {
+                response.getWriter().write("");
+            } else {
+                response.getWriter().write(outMessage.toEncryptedXml(this.configStorage));
+            }
+
             return;
         }
 
         response.getWriter().println("不可识别的加密类型");
-        return;
     }
 
     /**
      * 通过openid获得基本用户信息
      * 详情请见: http://mp.weixin.qq.com/wiki/14/bb5031008f1494a59c6f71fa0f319c66.html
      *
-     * @param response
-     * @param openid   openid
-     * @param lang     zh_CN, zh_TW, en
+     * @param openid openid
+     * @param lang   zh_CN, zh_TW, en
      */
-    @RequestMapping(value = "getUserInfo")
+    @RequestMapping(value = "/getUserInfo")
     public WxMpUser getUserInfo(HttpServletResponse response, @RequestParam(value = "openid") String openid, @RequestParam(value = "lang") String lang) {
         ReturnModel returnModel = new ReturnModel();
         WxMpUser wxMpUser = null;
@@ -129,11 +129,10 @@ public class CoreController extends GenericController {
      * 通过code获得基本用户信息
      * 详情请见: http://mp.weixin.qq.com/wiki/14/bb5031008f1494a59c6f71fa0f319c66.html
      *
-     * @param response
-     * @param code     code
-     * @param lang     zh_CN, zh_TW, en
+     * @param code code
+     * @param lang zh_CN, zh_TW, en
      */
-    @RequestMapping(value = "getOAuth2UserInfo")
+    @RequestMapping(value = "/getOAuth2UserInfo")
     public void getOAuth2UserInfo(HttpServletResponse response, @RequestParam(value = "code") String code, @RequestParam(value = "lang") String lang) {
         ReturnModel returnModel = new ReturnModel();
         WxMpOAuth2AccessToken accessToken;
@@ -157,10 +156,9 @@ public class CoreController extends GenericController {
      * 用code换取oauth2的openid
      * 详情请见: http://mp.weixin.qq.com/wiki/1/8a5ce6257f1d3b2afb20f83e72b72ce9.html
      *
-     * @param response
-     * @param code     code
+     * @param code code
      */
-    @RequestMapping(value = "getOpenid")
+    @RequestMapping(value = "/getOpenid")
     public void getOpenid(HttpServletResponse response, @RequestParam(value = "code") String code) {
         ReturnModel returnModel = new ReturnModel();
         WxMpOAuth2AccessToken accessToken;
