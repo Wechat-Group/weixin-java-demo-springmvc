8c1e2b29ad61b1f407b0a05f39774124bf1424ae
Binary Wang <binarywang@gmail.com>
2020-08-25 10:09:13 +0800

    :bug: 修复代码

--- src/main/java/com/github/weixin/demo/config/MenuConfig.java
+++ src/main/java/com/github/weixin/demo/config/MenuConfig.java
@@ -30,7 +30,7 @@ public class MenuConfig {
         WxMenuButton button1 = new WxMenuButton();
         button1.setType(MenuButtonType.VIEW);
         button1.setName("买家订单");
-        button1.setUrl(wxMpService.oauth2buildAuthorizationUrl("", "snsapi_base", ""));
+        button1.setUrl(wxMpService.getOAuth2Service().buildAuthorizationUrl("", "snsapi_base", ""));
 
         WxMenuButton button2 = new WxMenuButton();
         button2.setName("我是卖家");
@@ -38,22 +38,22 @@ public class MenuConfig {
         WxMenuButton button21 = new WxMenuButton();
         button21.setType(MenuButtonType.VIEW);
         button21.setName("我的订单");
-        button21.setUrl(wxMpService.oauth2buildAuthorizationUrl("", "snsapi_base", ""));
+        button21.setUrl(wxMpService.getOAuth2Service().buildAuthorizationUrl("", "snsapi_base", ""));
 
         WxMenuButton button22 = new WxMenuButton();
         button22.setType(MenuButtonType.VIEW);
         button22.setName("收入统计");
-        button22.setUrl(wxMpService.oauth2buildAuthorizationUrl("", "snsapi_base", ""));
+        button22.setUrl(wxMpService.getOAuth2Service().buildAuthorizationUrl("", "snsapi_base", ""));
 
         WxMenuButton button23 = new WxMenuButton();
         button23.setType(MenuButtonType.VIEW);
         button23.setName("发布商品");
-        button23.setUrl(wxMpService.oauth2buildAuthorizationUrl("", "snsapi_base", ""));
+        button23.setUrl(wxMpService.getOAuth2Service().buildAuthorizationUrl("", "snsapi_base", ""));
 
         WxMenuButton button24 = new WxMenuButton();
         button24.setType(MenuButtonType.VIEW);
         button24.setName("商品管理");
-        button24.setUrl(wxMpService.oauth2buildAuthorizationUrl("", "snsapi_base", ""));
+        button24.setUrl(wxMpService.getOAuth2Service().buildAuthorizationUrl("", "snsapi_base", ""));
 
         button2.getSubButtons().add(button21);
         button2.getSubButtons().add(button22);
